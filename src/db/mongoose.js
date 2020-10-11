503dd83ca443369664df3e4edc0908119b695df6
AlainMk <alainmukingiro.am@gmail.com>
2020-10-11 12:59:04 +0100

    Adding mongodb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/BlogApp', {
+mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
